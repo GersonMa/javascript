262269eac046b752dc299d5fbc57fbaac0c25e30
Gerson Maciel <gersonmabbcc@gmail.com>
2017-04-12 14:24:16 -0300

    Add files via upload

---
+++ ex14.1.js
@@ -0,0 +1,13 @@
+function evento_entrar()
+{
+alert("Seja Bem Vindo");
+}
+function eventro_sair()
+{
+alert("Até a proxima visita");
+}
+function evento_clique()
+{
+window.document.getElementById("id2").style.color="red";
+}
+
